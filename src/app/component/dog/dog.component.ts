74917801ba13f80a4ecb6cc3d7272ca74de5eea7
KevinGuzmanH <82849364+KevinGuzmanH@users.noreply.github.com>
2021-06-05 18:05:52 -0500

    Cambio de archivo de curriculum

--- src/app/component/dog/dog.component.ts
+++ src/app/component/dog/dog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TimelineMax } from 'gsap';
+import {gsap} from 'gsap/all';
 @Component({
   selector: 'app-dog',
   templateUrl: './dog.component.html',
@@ -10,6 +11,12 @@ export class DogComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    gsap.from('#heart',{
+      scale: 0.90,
+      duration: 2,
+      yoyo: true,
+      repeat: -1,
+    })
   }
 
 }
